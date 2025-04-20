70daad440103fbb3f9318139ddc3eda3e8396178
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-04-20 10:43:41 +0800

    chore: set expense date required.

--- lib/features/expense/pages/create_expense_page.dart
+++ lib/features/expense/pages/create_expense_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:money_expenses_tracker/common/extensions/context.dart';
 import 'package:money_expenses_tracker/common/widgets/common_progress_indicators.dart';
@@ -153,6 +154,9 @@ class CreateExpensePage extends HookConsumerWidget {
                     FormBuilderDateTimePicker(
                       name: CreateExpenseFormField.date.name,
                       decoration: const InputDecoration(labelText: 'Date'),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(),
+                      ]),
                       initialDate:
                           isUpdate ? currentExpense?.date : DateTime.now(),
                     ),
